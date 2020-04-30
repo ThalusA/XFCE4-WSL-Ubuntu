904455e9e47e7b59ce42083c2d40187f126b4814
Alexandre Monier <alexandre.monier@epitech.eu>
2020-04-30 16:58:55 +0200

    Pushing repo

---
+++ XUbuntu.bat
@@ -0,0 +1,2 @@
+start /B config.xlaunch
+start ubuntu.exe run "/etc/init.d/dbus start; export RUNLEVEL=3; export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0; xfce4-session; pkill '(gpg|ssh)-agent'; taskkill.exe /IM vcxsrv.exe /F;"
